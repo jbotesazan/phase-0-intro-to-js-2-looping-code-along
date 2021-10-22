291064c0706ae975577c2ca9379658785fcf5b87
Jason Botesazan <jbotesazan@gmail.com>
2021-10-22 00:14:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let c = 0; c < names.length; c++) {
+        messages.push(`Thank you, ${names[c]}, for the wonderful surprise gift!`)
+
+    }
+    return messages;
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
