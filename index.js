c0beb29bd77e2d6b4bfb052f2ae5bcb91486454c
Jason Botesazan <jbotesazan@gmail.com>
2021-11-28 15:43:46 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function writeCards(names, event) {
-    let messages = [];
-    for (let c = 0; c < names.length; c++) {
-        messages.push(`Thank you, ${names[c]}, for the wonderful surprise gift!`)
-
+function writeCards(name, event) {
+    let messages = []
+    for(let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
     }
     return messages;
 }
 
-function countDown(positiveNumber) {
-    while(positiveNumber >= 0) {
-        console.log(positiveNumber);
-        positiveNumber--;
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--;
     }
 }
